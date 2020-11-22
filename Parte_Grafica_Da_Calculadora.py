29ff246bf1014fab9c8f385559d450f56252391a
jesse abreu <jesse_cursopython3@hotmail.com>
2020-11-22 19:04:09 -0300

    Inicio_de_uma_agenda

--- Parte_Grafica_Da_Calculadora.py
+++ Parte_Grafica_Da_Calculadora.py
@@ -1,12 +1,30 @@
 from tkinter import *
 
-cal = Tk()
-cal.title('Calculadora')
-cal.configure(bg="#888")
-cal.geometry('500x500')
+age=Tk()
+age.geometry('500x500')
+age.configure(bg='#599')
+age.title("Agenda")
+
+def salva():
+    arq=open('agenda.txt', 'a')
+    arq.write('Nome: ')
+    arq.write(xnome.get())
+    arq.write('\n')
+    arq.write('Contato: ')
+    arq.write(xfone.get())
+    arq.write('\n')
+    arq.write('\n')
+    arq.close()
+
+Label(age,text='Adiciona Nome',).place(x=10,y=10)
+xnome=Entry(age)
+xnome.place(x=130,y=10)
+
+Label(age,text='Adiciona Telefone',).place(x=10,y=50)
+xfone=Entry(age)
+xfone.place(x=130,y=50)
+
+Button(age,text='Salvar',command=salva).place(x=10,y=90)
 
 
-
-
-
-cal.mainloop()
+age.mainloop()
